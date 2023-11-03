dcd31a42ad516f22eedd8f2b155386d1fb2d9934
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-11-03 10:57:55 +0200

    рендер

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+let galleryImages = "";
+
+for (const item of galleryItems) {
+  galleryImages += `<a class="gallery_item" href="${item.original}">
+    <img class="gallery_image" src="${item.preview}" alt="${item.description}" />
+  </a>`;
+}
+
+gallery.insertAdjacentHTML("beforeend", galleryImages);
