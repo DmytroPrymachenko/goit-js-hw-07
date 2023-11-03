61f768e304c84ee4b14fa5f141e6b74e7f02d668
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-11-03 16:46:10 +0200

    2dz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,63 @@
 import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
+console.log(basicLightbox);
 // Change code below this line
 
 console.log(galleryItems);
-
+let instance = null;
 const gallery = document.querySelector(".gallery");
-let galleryImages = "";
 
-for (const item of galleryItems) {
-  galleryImages += `<a class="gallery_item" href="${item.original}">
-    <img class="gallery_image" src="${item.preview}" alt="${item.description}" />
-  </a>`;
-}
+const galleryItemsEl = galleryItems.map((galleryItemInfo) => {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${galleryItemInfo.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItemInfo.preview}"
+          data-source="${galleryItemInfo.original}"
+          alt="${galleryItemInfo.description}"
+        />
+      </a>
+    </li>
+  `;
+});
 
-gallery.insertAdjacentHTML("beforeend", galleryImages);
+gallery.insertAdjacentHTML("beforeend", galleryItemsEl.join(""));
+
+const onGalleryItemClick = (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalPath = event.target.dataset.source;
+  console.dir(originalPath);
+
+  const basicLightboxOptions = {
+    onClose() {
+      document.removeEventListener("keydown", onDocumentKeyPress);
+    },
+  };
+
+  instance = basicLightbox.create(
+    `
+    <img src="${originalPath}" width="800" height="600">
+`,
+    basicLightboxOptions
+  );
+
+  instance.show();
+
+  document.addEventListener("keydown", onDocumentKeyPress);
+};
+
+const onDocumentKeyPress = ({ code }) => {
+  console.log(code);
+
+  if (code === "Escape") {
+    instance.close();
+  }
+};
+
+gallery.addEventListener(`click`, onGalleryItemClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems.map((galleryInfo) => {
+  return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${galleryInfo.original}">
+    <img
+      class="gallery__image"
+      src="${galleryInfo.preview}"
+      alt="${galleryInfo.description}"
+    />
+  </a>
+</li>
+  `;
+});
+gallery.insertAdjacentHTML("beforeend", galleryItemsEl.join(""));
+
+const onGalleryItemClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+};
+
+// const SimpleLightboxOptions = {
+// new  SimpleLightbox ( '.gallery', { } ) ;
+
+// };
+let gallerys = new SimpleLightbox(".gallery a", {
+  captionsData: `alt`,
+  captionDelay: "250",
+});
